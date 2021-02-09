c3152ff7fca9e17a1c60820ecca136a28c67a617
yoshiki <yoshiki.hashimoto@matrixflow.jp>
2021-02-10 01:43:09 +0900

    add lambdaExecution before DockerBuild

--- typescript/codepipeline-eb-blue-green-deploy/index.ts
+++ typescript/codepipeline-eb-blue-green-deploy/index.ts
@@ -1,8 +1,11 @@
+import * as path from "path";
+
 import * as cdk from "@aws-cdk/core";
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as lambda from '@aws-cdk/aws-lambda';
+import { PythonFunction } from '@aws-cdk/aws-lambda-python';
 import * as iam from '@aws-cdk/aws-iam';
 
 import { getParams } from './params';
@@ -16,26 +19,6 @@ export class PipelineStack extends cdk.Stack {
             pipelineName: "MyPipeline"
         });
 		const params = getParams()
-
-		const buildRole = new iam.Role(this, 'BuildRole', {
-			assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com')
-		})
-		buildRole.addManagedPolicy(iam.ManagedPolicy.fromManagedPolicyArn(this, 'BuildRoleToAccessECR', 'arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess'))
-
-		// to build docker in CodeBuild, set priviledged True
-        const project = new codebuild.PipelineProject(this, 'MyProject', {
-			environment: {
-				buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2,
-				privileged: true
-			},
-			environmentVariables: {
-				"AWS_ACCOUNT": {
-					type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-					value: params.awsAccountId,
-				}
-			},
-			role: buildRole
-		});
 
         // S3 location
         const sourceOutput = new codepipeline.Artifact();
@@ -54,6 +37,28 @@ export class PipelineStack extends cdk.Stack {
         });
 
 		/**
+		 * lambda to get current date as docker-image-tag
+		 *
+		 * lambda returns '2021-01-01'.
+		 */
+		const lambdaCurrentDate = new PythonFunction(this, 'lambdaCurrentDate', {
+			entry: path.resolve(__dirname, './lambda'),
+			index: 'get_execute_date.py',
+			handler: 'handler',
+			runtime: lambda.Runtime.PYTHON_3_8,
+			timeout: Duration.seconds(120),
+		})
+		cdk.Tags.of(lambdaCurrentDate).add("runtime", "python")
+		const getCurrentDateAction = new codepipeline_actions.LambdaInvokeAction({
+			actionName: 'getCurrentDate',
+			lambda: lambdaCurrentDate
+		})
+		pipeline.addStage({
+            stageName: 'GetDockerImageTag',
+            actions: [getCurrentDateAction],
+        });
+
+		/**
 		 * approval action
 		 */
 		const approvalAction = new codepipeline_actions.ManualApprovalAction({
@@ -65,6 +70,30 @@ export class PipelineStack extends cdk.Stack {
 		/**
 		 * deploy action
 		 */
+		const buildRole = new iam.Role(this, 'BuildRole', {
+			assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com')
+		})
+		buildRole.addManagedPolicy(iam.ManagedPolicy.fromManagedPolicyArn(this, 'BuildRoleToAccessECR', 'arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess'))
+
+		// to build docker in CodeBuild, set priviledged True
+        const project = new codebuild.PipelineProject(this, 'MyProject', {
+			environment: {
+				buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2,
+				privileged: true
+			},
+			environmentVariables: {
+				"AWS_ACCOUNT": {
+					type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+					value: params.awsAccountId,
+				},
+				"IMAGE_TAG": {
+					type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+					value: getCurrentDateAction.variable('current_date')
+				}
+			},
+			role: buildRole
+		});
+
 		const buildAction = new codepipeline_actions.CodeBuildAction({
             actionName: 'CodeBuild',
             project,
