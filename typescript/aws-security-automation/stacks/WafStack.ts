1aa6d4b0ecab3170e3dc6012b86a973e11e54d02
gsy0911 <yoshiki0911@gmail.com>
2021-03-25 17:47:22 +0900

    #3 create kinesis-firehose and S3

--- typescript/aws-security-automation/stacks/WafStack.ts
+++ typescript/aws-security-automation/stacks/WafStack.ts
@@ -1,12 +1,48 @@
 import * as cdk from "@aws-cdk/core";
 import * as wafv2 from "@aws-cdk/aws-wafv2";
+import * as firehose from "@aws-cdk/aws-kinesisfirehose";
+import * as s3 from '@aws-cdk/aws-s3';
+import * as iam from '@aws-cdk/aws-iam';
+
 import { IWafv2Stack } from '../IParameters';
 
 export class WafStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, params: IWafv2Stack, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		// WebACLを作成
+		/** bucket to store log from Firehose */
+		const logS3 = new s3.Bucket(this, "log-bucket-aws-cdk-example", {
+			bucketName: "log-bucket-aws-cdk-example",
+			autoDeleteObjects: true,
+			removalPolicy: cdk.RemovalPolicy.DESTROY
+		})
+		const firehoseAccessS3Role = new iam.Role(this, `firehose-access-s3-role`, {
+			roleName: `firehose-access-s3-role`,
+			assumedBy: new iam.ServicePrincipal('firehose.amazonaws.com')
+		})
+
+		firehoseAccessS3Role.addToPolicy(new iam.PolicyStatement({
+			effect: iam.Effect.ALLOW,
+			resources: [`arn:aws:s3:::${logS3.bucketName}`],
+			actions: [
+				's3:*',
+			]
+		}))
+
+		/**
+		 * Kinesis Firehose to stream logs from WAFv2
+		 *
+		 * stream name must start with `aws-waf-logs-`
+		 */
+		const wafLogFirehose = new firehose.CfnDeliveryStream(this, "SampleFirehose", {
+			deliveryStreamName: "aws-waf-logs-sample",
+			s3DestinationConfiguration: {
+				bucketArn: logS3.bucketArn,
+				roleArn: firehoseAccessS3Role.roleArn
+			}
+		})
+
+		// WebACL
 		const webAcl = new wafv2.CfnWebACL(this, "SampleWafAcl", {
 		defaultAction: { allow: {} },
 		name: "sample-waf-web-acl",
@@ -98,7 +134,8 @@ export class WafStack extends cdk.Stack {
 			cloudWatchMetricsEnabled: true,
 			metricName: "sample-waf-web-acl",
 			sampledRequestsEnabled: true
-		}
+		},
+
 		});
 	}
 }
