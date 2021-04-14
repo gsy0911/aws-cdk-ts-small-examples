d74b2fc21e861e68d04e25dcc06e4822c7b156fc
gsy0911 <yoshiki0911@gmail.com>
2021-04-14 16:15:07 +0900

    add ec2

---
+++ typescript/ec2-ismv2/IParameters.ts
@@ -0,0 +1,8 @@
+export interface IParameters {
+	vpcCidr: string
+}
+
+
+export const defaultParams: IParameters = {
+	vpcCidr: "192.168.5.0/24"
+}
---
+++ typescript/ec2-ismv2/index.ts
@@ -0,0 +1,9 @@
+import * as cdk from "@aws-cdk/core";
+import {Ec2Imdsv2Stack} from './Ec2Ismv2Stack';
+import {IParameters} from './IParameters';
+import {params} from './params';
+
+
+const app = new cdk.App();
+new Ec2Imdsv2Stack(app, "Ec2Ismv2", params, {description: "ts-example"});
+app.synth();
