b55b51a2d6799f855c09c0b931a4f1e968ae3b12
gsy0911 <yoshiki0911@gmail.com>
2021-04-14 18:14:25 +0900

    move wafv2

--- typescript/stacks/index.ts
+++ typescript/stacks/index.ts
@@ -1,2 +1,3 @@
 export {VpcEc2Stack, IVpcEc2} from './VpcEc2Stack';
 export {CloudTrailStack, ICloudTrailStack} from './CloudTrailStack';
+export {Wafv2ApigwStack, IWafv2ApigwStack} from './Wafv2ApigwStack';
