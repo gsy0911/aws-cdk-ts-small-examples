51a1b8deab64b2fee8ee4bb55d4393e515d8ee48
gsy0911 <yoshiki0911@gmail.com>
2021-05-10 17:00:36 +0900

    remove multiple fargate

--- typescript/ecs-fargate/index.ts
+++ typescript/ecs-fargate/index.ts
@@ -1,5 +1,5 @@
 import * as cdk from "@aws-cdk/core";
-import {EcsFargateStack, EcrEcsFargateStack, EcrEcsMultipleFargateElbStack, EcrEcsSingleFargateElbStack} from '../stacks';
+import {EcsFargateStack, EcrEcsFargateStack, EcrEcsSingleFargateElbStack} from '../stacks';
 import {params} from './params';
 
 
@@ -7,6 +7,5 @@ const app = new cdk.App();
 new EcsFargateStack(app, "EcsFargate", params, {description: "ts-example: from example", env: params.env});
 new EcrEcsFargateStack(app, "EcrEcsFargate", params, {description: "ts-example: from defined docker", env: params.env});
 new EcrEcsSingleFargateElbStack(app, "EcrEcsSingleFargateElb", params, {description: "ts-example: from defined docker with alb", env: params.env});
-new EcrEcsMultipleFargateElbStack(app, "EcrEcsFargateElb", params, {description: "ts-example: from defined docker with alb", env: params.env});
 
 app.synth();
--- typescript/stacks/index.ts
+++ typescript/stacks/index.ts
@@ -4,7 +4,6 @@ export {Wafv2ApigwStack, IWafv2ApigwStack} from './Wafv2ApigwStack';
 export {BatchSfnStack, IBatchLogSfn} from './BatchLogSfnStack';
 export {EcsFargateStack, IEcsFargate} from './EcsFargateStack';
 export {EcrEcsFargateStack, IEcrEcsFargate} from './EcrEcsFargateStack';
-export {EcrEcsMultipleFargateElbStack, IEcrEcsFargateElb} from './EcrEcsMultipleFargateElbStack';
 export {EventBridgeTriggeredEcsSingleFargatePipeline, IEventBridgeTriggeredEcsFargatePipeline} from './EventBridgeTriggeredEcsSingleFargatePipeline';
 export {EcrEcsSingleFargateElbStack} from './EcrEcsSingleFargateElbStack';
 export {CognitoStack, ICognitoStack} from './CognitoStack';
