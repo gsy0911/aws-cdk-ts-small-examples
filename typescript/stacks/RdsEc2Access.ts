1029558b5e2cec0bf16d2892c204ba143ad11410
gsy0911 <yoshiki0911@gmail.com>
2021-06-22 14:38:11 +0900

    #10 able to access from only vpc

--- typescript/stacks/RdsEc2Access.ts
+++ typescript/stacks/RdsEc2Access.ts
@@ -28,69 +28,54 @@ export class RdsEc2AccessStack extends cdk.Stack {
 
 			]
 		})
-		const securityGroup = new ec2.SecurityGroup(this, `${id}-security-group`, {
+		const securityGroup = new ec2.SecurityGroup(this, "security-group", {
 			vpc: vpc,
 			securityGroupName: `${id}-security-group`,
 			allowAllOutbound: true,
 		})
-		const instanceRole = new iam.Role(this, `${id}-instance-role`, {
+		const instanceRole = new iam.Role(this, "instance-role", {
 			roleName: `${id}-instance-role`,
 			assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
 		})
 		instanceRole.addManagedPolicy(
 			iam.ManagedPolicy.fromManagedPolicyArn(
-				this, `AmazonEC2ContainerServiceforEC2Role-${id}`, "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+				this, "AmazonEC2ContainerServiceforEC2Role", "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 			)
 		)
 		/** Add managed policy to use SSM */
 		instanceRole.addManagedPolicy(
 			iam.ManagedPolicy.fromManagedPolicyArn(
-				this, `AmazonEC2RoleforSSM-${id}`, "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+				this, "AmazonEC2RoleforSSM", "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 			)
 		)
-		const instanceProfile = new iam.CfnInstanceProfile(this, `${id}-instance-profile`, {
+		const instanceProfile = new iam.CfnInstanceProfile(this, "instance-profile", {
 			instanceProfileName: `${id}-instance-profile`,
 			roles: [instanceRole.roleName]
 		})
 
-		const ec2Instance = new ec2.Instance(this, "example-instance", {
+		const ec2Instance = new ec2.Instance(this, "accessible-instance", {
 			instanceType: new ec2.InstanceType("t3.nano"),
 			vpc: vpc,
 			machineImage: ec2.MachineImage.latestAmazonLinux(),
-			initOptions: {
-				configSets: ["default"],
-				timeout: cdk.Duration.minutes(30)
-			},
-			init: ec2.CloudFormationInit.fromConfigSets({
-				configSets: {
-					default: ["aptInstall", "configuration"]
-				},
-				configs: {
-					aptInstall: new ec2.InitConfig([
-						ec2.InitPackage.yum("jq")
-					]),
-					configuration: new ec2.InitConfig([
-						ec2.InitCommand.shellCommand('mkdir -p /opt/local/bin')
-					])
-				}
-			}),
 			role: instanceRole
 		})
 
 		/**
 		 * > By default, the master password will be generated and stored in AWS Secrets Manager with auto-generated description.
 		 */
-		const dbCluster = new rds.DatabaseCluster(this, "postgresql", {
-			engine: rds.DatabaseClusterEngine.auroraPostgres({version: rds.AuroraPostgresEngineVersion.VER_10_14}),
-			instanceProps: {
-				instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
-				vpcSubnets: {
-					subnetType: ec2.SubnetType.PRIVATE
-				},
-				vpc
+		const dbInstance = new rds.DatabaseInstance(this, "postgresql", {
+			engine: rds.DatabaseInstanceEngine.postgres({
+				version: rds.PostgresEngineVersion.VER_10_4
+			}),
+			instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+			vpcSubnets: {
+				subnetType: ec2.SubnetType.PRIVATE
 			},
-			defaultDatabaseName: "cdkexample"
+			databaseName: "cdkexample",
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			vpc
 		})
+		dbInstance.connections.allowFrom(ec2Instance, ec2.Port.tcp(5432));
 
 	}
 }
