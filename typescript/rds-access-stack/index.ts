1ddc0f0a6c679eed3a7a10460c7a7ef11ddb9a85
gsy0911 <yoshiki0911@gmail.com>
2021-06-22 16:27:41 +0900

    #10 add access rds via iam

--- typescript/rds-access-stack/index.ts
+++ typescript/rds-access-stack/index.ts
@@ -1,5 +1,5 @@
 import * as cdk from "@aws-cdk/core";
-import {RdsEc2AccessStack, IRdsEc2AccessStack} from '../stacks';
+import {RdsEc2AccessStack, RdsEc2IamAccessStack, IRdsEc2AccessStack} from '../stacks';
 
 
 export const params: IRdsEc2AccessStack = {
@@ -8,5 +8,6 @@ export const params: IRdsEc2AccessStack = {
 
 
 const app = new cdk.App();
-new RdsEc2AccessStack(app, "rds-ec2", params, {description: "ts-example"});
+// new RdsEc2AccessStack(app, "rds-ec2", params, {description: "ts-example"});
+new RdsEc2IamAccessStack(app, "rds-ec2", params, {description: "ts-example"});
 app.synth();
