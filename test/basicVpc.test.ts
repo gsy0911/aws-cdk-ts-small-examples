f054a737fbcde750fc3823f8d6b7f82d4301c892
yoshiki <yoshiki0911@gmail.com>
2021-07-18 00:43:11 +0900

    #13 not working well

--- test/basicVpc.test.ts
+++ test/basicVpc.test.ts
@@ -1,4 +1,4 @@
-import {expect, countResources, matchTemplate, MatchStyle} from "@aws-cdk/assert";
+import {expect as expectCDK, countResources, haveResource, matchTemplate, MatchStyle} from "@aws-cdk/assert";
 import * as cdk from '@aws-cdk/core';
 import {VpcStack} from '../typescript/stacks/BasicVpc';
 
@@ -7,5 +7,5 @@ const app = new cdk.App()
 const stack = new VpcStack(app, "example", {name: "test", environment: "test"})
 
 test('vpc', () => {
-	expect(stack).to(countResources("AWS::EC2::VPC", 1))
+	expectCDK(stack).to(countResources("AWS::EC2::VPC", 1))
 })
