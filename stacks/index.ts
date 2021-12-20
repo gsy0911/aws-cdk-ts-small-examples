23a2dcc812b2b4783e791aac58ed75c4d3717e34
gsy0911 <yoshiki0911@gmail.com>
2021-12-21 00:30:34 +0900

    #21 delete un-used files

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,13 +1,7 @@
 export {VpcEc2Stack, IVpcEc2} from './VpcEc2Stack';
 export {CloudTrailStack, ICloudTrailStack} from './CloudTrailStack';
 export {Wafv2ApigwStack, IWafv2ApigwStack} from './Wafv2ApigwStack';
-export {BatchSfnStack, IBatchLogSfn} from './BatchLogSfnStack';
-export {EcsFargateStack, IEcsFargate} from './EcsFargateStack';
-export {EcrEcsFargateStack, IEcrEcsFargate} from './EcrEcsFargateStack';
 export {EventBridgeTriggeredEcsSingleFargatePipeline, IEventBridgeTriggeredEcsFargatePipeline} from './EventBridgeTriggeredEcsSingleFargatePipeline';
-export {EcrEcsSingleFargateElbStack} from './EcrEcsSingleFargateElbStack';
-export {EcrEcsMultipleFargateElbStack1} from './EcrEcsMultipleFargateElbStack1';
-export {EcrEcsMultipleFargateElbStack2} from './EcrEcsMultipleFargateElbStack2';
 export {CognitoStack, ICognitoStack} from './CognitoStack';
 export {SamExample, ISamExample} from './SamExample';
 export {
